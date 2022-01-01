e93db18cfedd9efd30420b48d14092488d6cabe5
Amit <hey@outlook.com>
2022-01-01 20:57:53 +0530

    resolved error

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,15 @@
 import Head from "next/head"
 import { Component } from 'react'
 import { attributes, react as HomeContent } from '../content/home.md';
+import Script from 'next/script'
 
 export default class Home extends Component {
   render() {
     let { title, cats } = attributes;
     return (
       <>
+        <Script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></Script>
         <Head>
-          <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
         </Head>
         <article>
           <h1>{title}</h1>
